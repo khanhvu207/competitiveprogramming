91ba2bdbe6f9832283c3a1d9ccb61f54109e32bd
Vũ Nguyễn Khánh <khanhvu2072001@gmail.com>
2020-04-17 18:05:05 +0200

    Create sieve.cpp

---
+++ tools/sieve.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define ll long long
+using namespace std;
+
+const int MAXSIEVE=31622778;
+bitset<MAXSIEVE> notComposite;
+vector<int> prime;
+
+ll countDivisors(ll x){
+	int res=1LL;
+	for(int p:prime){
+		if(p*p*1LL>x) break;
+		int cnt=1LL;
+		while(x%p==0) ++cnt,x/=p;
+		res*=cnt;
+	}
+	if(x>1) res*=2LL;
+	return res;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	notComposite.flip();
+	for(int i=2;i*i<MAXSIEVE;++i) if(notComposite.test(i)) for(int j=i*i;j<MAXSIEVE;j+=i) notComposite.reset(j);
+	for(int i=2;i<MAXSIEVE;++i) if(notComposite.test(i)) prime.emplace_back(i);
+}
